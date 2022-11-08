4766e592f853de27e9396d47d8ba9646cc1976f5
Jungi Park <byjungy93@gmail.com>
2022-11-08 09:50:19 +0900

    [Finished] 002_짝지어 제거하기_d3

--- programmers/002_짝지어 제거하기.js
+++ programmers/002_짝지어 제거하기.js
@@ -1,13 +1,19 @@
 function solution(s)
 {
- // failed one effciency test case
+ // initializing an empty array for stack
  let stack = [];
+ 
+ // looping through for looing
  for(let i=0;i< s.length; i++){
+     // pushing all the alphabet of s
     stack.push(s[i]);
+     // if last element and second to last are same 
+     // pop the elements 
      if(stack[stack.length-1] === stack[stack.length-2]){
          stack.pop()
          stack.pop()
      }
  }
+    // if stack is empty, return 1
     return (stack.length === 0 ? 1 : 0)
 }
