fae6f1ad9a2fa7128d180c065a29a940f84c0386
Jungi Park <byjungy93@gmail.com>
2022-11-13 20:43:36 +0900

    [Finished] 001_모의고사_d2

--- programmers/001_모의고사.js
+++ programmers/001_모의고사.js
@@ -1,24 +1,26 @@
 function solution(answers) {
     
-    console.log("check answers", answers.length)
+    let answer = []
+    let a1 = [1,2,3,4,5]
+    let a2 = [2,1,2,3,2,4,2,5]
+    let a3 = [3,3,1,1,2,2,4,4,5,5]
     
-    var answer = [];
-    var a1 = [1, 2, 3, 4, 5];
-    var a2 = [2, 1, 2, 3, 2, 4, 2, 5];
-    var a3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-
-    // i= 0 to i = 4, a1.length = 5
-    var a1c = answers.filter((a,i)=> a === a1[i%a1.length]).length;
-    // i= 0 to i = 7, a2.length = 8 
-    var a2c = answers.filter((a,i)=> a === a2[i%a2.length]).length;
-    // i= 0 to i = 9, a3.length = 10
-    var a3c = answers.filter((a,i)=> a === a3[i%a3.length]).length;
-    var max = Math.max(a1c,a2c,a3c);
-
-    if (a1c === max) {answer.push(1)};
-    if (a2c === max) {answer.push(2)};
-    if (a3c === max) {answer.push(3)};
-
-
-    return answer;
+    // find number of right answer from p1
+    let a1c = answers.filter((a,i) => a === a1[i%a1.length]).length
+    
+    // find number of right answer from p2
+    let a2c = answers.filter((a,i) => a === a2[i%a2.length]).length
+    
+    // find number of right answer from p3
+    let a3c = answers.filter((a,i) => a === a3[i%a3.length]).length
+    
+    // find what is max number of right answer
+    const max = Math.max(a1c,a2c, a3c)
+    
+    // find who get most of the answers right
+    if(a1c === max) answer.push(1)
+    if(a2c === max) answer.push(2)
+    if(a3c === max) answer.push(3)
+    
+    return answer
 }
