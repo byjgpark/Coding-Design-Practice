4522770c8374bdfe1254ef4b8714c62b8f251873
Jungi Park <byjungy93@gmail.com>
2022-11-16 18:56:51 +0900

    [Working] 002_소수 찾기 d2

--- programmers/002_소수 찾기.js
+++ programmers/002_소수 찾기.js
@@ -1,4 +1,6 @@
 function solution(numbers) {
+
+    // tried to study the solution from https://sumin-k.medium.com/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-javascript-%EC%99%84%EC%A0%84-%ED%83%90%EC%83%89-%EC%86%8C%EC%88%98-%EC%B0%BE%EA%B8%B0-1fdcdca4f59b
     var answer = 0;
     console.log("Check", numbers)
     return answer;
