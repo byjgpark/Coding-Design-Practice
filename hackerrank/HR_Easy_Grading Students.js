9b9775ec95d5a5df6195b1d3426cf9fe04a49174
Jungi Park <byjungy93@gmail.com>
2022-11-29 21:40:01 +0900

    [Working] Easy_Grading Students_d3

--- hackerrank/HR_Easy_Grading Students.js
+++ hackerrank/HR_Easy_Grading Students.js
@@ -1,18 +1,29 @@
 function multipleOfFive(x) {
 
-    
     // i = 0 => x = grades[0] = 73
+    // i = 1 => x = grades[1] = 67
+    // i = 2 => x = grades[2] = 38
+    // i = 3 => x = grades[3] = 33
     let counter = 0;
     while (x % 5 != 0) {
         x++;
         // i = 0 => x = 74
         // i = 0 => x = 75
+        
+        // i = 1 => x = 68
+        // i = 1 => x = 69
+        // i = 1 => x = 70
         counter++;
-        // counter = 1
-        // counter = 2
+        // i = 0 => counter = 1
+        // i = 0 => counter = 2
+
+        // i = 1 => counter = 1
+        // i = 1 => counter = 2
+        // i = 1 => counter = 3
     }
     return counter;
-    // return counter = 75
+    // return i = 0 => counter = 75
+    // return i = 1 => counter = 70
 }
 
 function gradingStudents(grades) {
@@ -21,16 +32,21 @@ function gradingStudents(grades) {
     for (let i = 0; i < grades.length; i++) {
         
         let difference = multipleOfFive(grades[i]);
-        // i = 0 => grades[0] = 73, counter = 2
+        // i = 0 => grades[0] = 73, difference = 2
+        // i = 1 => grades[1] = 67, difference = 3
         
         let finalGrade = difference + grades[i];
-        // finalGrade = 2 + 73
+        // i = 0 => finalGrade = 2 + 73
+        // i = 1 => finalGrade = 3 + 67
         
         if ((difference < 3) && (finalGrade >= 40)) {
             roundedGrades.push(finalGrade);
             // i = 0, roundedGrades = [75, ]
+            // i = 2, roundedGrades = [75, 67, 40]
         } else {
             roundedGrades.push(grades[i]);
+            // i = 1, roundedGrades = [75, 67]
+            // i = 3, roundedGrades = [75, 67, 40, 33]
         }
     }
     return roundedGrades;
