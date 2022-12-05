34af8324080824cbba7ec44ab9bfada10bd311f1
Jungi Park <byjungy93@gmail.com>
2022-12-05 20:27:57 +0900

    [Finished] Easy_Grading Students_d6

--- hackerrank/HR_Easy_Grading Students.js
+++ hackerrank/HR_Easy_Grading Students.js
@@ -1,5 +1,7 @@
 function gradeDiff(grade){
      
+    // incrementing grade and counter 
+    // until grdae is divisible by 5
     let counter = 0
     while(grade % 5 != 0){
         grade++
@@ -10,29 +12,36 @@ function gradeDiff(grade){
 
 function gradingStudents(grades) {
    
-   // upload comments
+   // answer array
    let answer = []
-   // console.log("check grades", grades)    
    
+   // looping though the number of grades
    for(let i = 0; i< grades.length; i++){
+
+       // getting difference from the next multiple of 5
        let difference = gradeDiff(grades[i])
+
+       // getting the next multiple of 5
        let nextNum = grades[i] + difference
-       
-       console.log("check difference", difference)
-       
-       // difference < 3 
+           
+       // if difference is less than 3 and grades[i] is greater than 38
+       // push next number to answer
        if( difference < 3 && grades[i] >= 38){
            answer.push(nextNum)
        }
+
+       // if difference is greater than & equal 3 and grades[i] is greater than 38
+       // push grades[i] to answer
        else if ( difference >= 3 && grades[i] >= 38){
            answer.push(grades[i])
        }
+
+       // if grades[i] is less than & equal to 38
+       // push grades[i] to answer
        else if ( grades[i] <= 38){
            answer.push(grades[i])   
        }
    }
-   
-   console.log('check grades', answer)
    
    return answer
 }
