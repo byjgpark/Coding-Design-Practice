49cf12418e889d8c1eec15f015376ff5f2f1f620
Jungi Park <byjungy93@gmail.com>
2023-01-08 18:43:37 +0900

    [Working] Medium_encryption_d20

--- hackerrank/HR_Medium_Encryption.js
+++ hackerrank/HR_Medium_Encryption.js
@@ -3,4 +3,5 @@ function encryption(s) {
     const ceil = Math.ceil(Math.sqrt(s.length));
     console.log("check s ", s, "check s length", s.length)
     console.log("check ceil", ceil)   
+    
 }
