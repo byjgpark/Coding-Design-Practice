bd2d18718cda0f410299e688d7048a2e600d2d74
Jungi Park <byjungy93@gmail.com>
2023-03-21 09:19:39 +0900

    [Finished] Easy_Electronic Shop_d2

--- hackerrank/HR_Easy_Electronic Shop.js
+++ hackerrank/HR_Easy_Electronic Shop.js
@@ -1,20 +1,19 @@
 function getMoneySpent(keyboards, drives, b) {
-    
-    console.log("check keyboards", keyboards)
-    
-    console.log("check drives", drives)
-    
-    let maxValue = -1;
-    for (let drive of drives) {
-        for(let keyboard of keyboards) {
-            
-            console.log("check drive ", drive, "check keyboard", keyboard)
-            
-            let cost = drive + keyboard;
-            if(cost > maxValue && cost <= b) {
-                maxValue = cost;
-            }
+    // initialize -1 for maxNum
+    let maxNum = -1
+    // Iterate through all the combination from keyboard & drive
+    for(const keyboard of keyboards){
+        for(const drive of drives){
+            // suming up all the combination from keyboard & drive
+            let sum = keyboard + drive
+            // if the sum is bigger than the maximum and the sum 
+            // is bigger than & equal to the b
+            if( sum > maxNum && sum <= b){
+            // Assign sum to maxNum
+                maxNum = sum
+            }    
         }
     }
-    return maxValue;
+    // return maxNum
+    return maxNum
 }
