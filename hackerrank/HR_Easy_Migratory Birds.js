841d05b8166661122cb026b17a4161ea1686e462
Jungi Park <byjungy93@gmail.com>
2023-03-25 11:20:21 +0900

    [Working] Easy_Migratory Birds_d5

--- hackerrank/HR_Easy_Migratory Birds.js
+++ hackerrank/HR_Easy_Migratory Birds.js
@@ -1,19 +1,24 @@
 function migratoryBirds(arr) {
-
-    console.log("check arr", arr)
-   
+    
     let max = 1;
-    let counter = max
+    let counter = 1;
+    // sorting the array in ascending order
+    arr.sort()
+    let type = 0
     
-    console.log("this is sorted arr", arr.sort())
-    
-    for(let i = 0; i < arr.length; i++){
+    // Looping through the first index of occurrence number in the array
+    for(let i = 0; i < arr.length; i = arr.lastIndexOf(arr[i])+1){
         
+        // finding the difference between first index  
+        // and last index of the array value 
         max = arr.lastIndexOf(arr[i]) - arr.indexOf(arr[i])
         
-        console.log("check first index of something", max)       
+        // if max is bigger than counter
+        // assign max to counter as well as arr[i] to type
+        if( max > counter){
+            counter = max;
+            type = arr[i]
+        }
     }
-    
-    console.log("check counter", counter) 
-    
+    return type
 }
