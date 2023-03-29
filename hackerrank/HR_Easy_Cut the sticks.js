e72c0e9f011acc9bf8d70b09fbc19f3a71583968
Jungi Park <byjungy93@gmail.com>
2023-03-29 09:24:02 +0900

    [Working] Easy_Cut_the_sticks_d2

--- hackerrank/HR_Easy_Cut the sticks.js
+++ hackerrank/HR_Easy_Cut the sticks.js
@@ -1,1 +1,11 @@
-  
+function cutTheSticks(arr) {
+    // Write your code here
+    
+    console.log("check arr", arr)
+    
+    console.log("check min value", Math.min(...arr))
+    
+    let minNum = Math.min(...arr)
+    
+    console.log("check minNum in the array", arr.map( element => element - minNum))
+}
