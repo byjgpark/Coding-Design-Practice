e843b0b30d5e80a7344dcd802d5b72435bff09f2
Jungi Park <byjungy93@gmail.com>
2023-03-30 08:34:13 +0900

    [Working] Easy_Cut_the_sticks_d3

--- hackerrank/HR_Easy_Cut the sticks.js
+++ hackerrank/HR_Easy_Cut the sticks.js
@@ -1,11 +1,26 @@
 function cutTheSticks(arr) {
     // Write your code here
     
-    console.log("check arr", arr)
+  // Write your code here
     
-    console.log("check min value", Math.min(...arr))
+  console.log("check arr", arr)
     
-    let minNum = Math.min(...arr)
-    
-    console.log("check minNum in the array", arr.map( element => element - minNum))
+  console.log("check min value", Math.min(...arr))
+  
+  let minNum = Math.min(...arr)
+  
+  let subtrack = arr.map(element => element - minNum)
+  
+  console.log("check subtrack in the array", subtrack)
+  
+  let i = 0;
+  while(i < subtrack.length){
+      if(subtrack[i] === 0){
+          subtrack.splice(i,1)
+      }else{
+          ++i;
+      }
+  }
+  
+  console.log("check subtrack's length at the end ", subtrack.length)
 }
