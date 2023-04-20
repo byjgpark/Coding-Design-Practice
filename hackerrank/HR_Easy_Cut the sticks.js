708e8ec9fe40627780085a5b6a2fde93e92d77d3
Jungi Park <byjungy93@gmail.com>
2023-04-20 19:23:00 +0900

    [Working] Easy_Cut_the_sticks_d12

--- hackerrank/HR_Easy_Cut the sticks.js
+++ hackerrank/HR_Easy_Cut the sticks.js
@@ -1,40 +1,31 @@
 function cutTheSticks(arr) {
     
-    let output = [];
+    // Write your code here
     
     console.log("check arr", arr)
     
-    while (arr.length != 0) {
+    let sizeOfArray = []
+    
+    // console.log('check minValue', minValue)
+    
+    sizeOfArray.push(arr)
+    
+    while(arr != 0){
         
-        let minVal = Math.min(...arr);
+        let minValue = Math.min(...arr)
         
-        // console.log("check minVal", minVal)
-        
-        output.push(arr.length);
-        
-        for (let i = 0; i < arr.length; i++) {
+        for(let i = 0; i < arr.length; i++){
             
-            let sub = arr[i] - minVal;
+            let sub = arr[i] - minValue
             
-            console.log("check sub", sub)
-            
-            // console.log("check i before decrement ", i) 
-
-            
-            if (sub === 0) {
-                console.log("check i before decrement ", i) 
-                arr.splice(i, 1);
-                i -= 1;
-            
-                console.log("check i after decrement ", i)
-                console.log("check arr inside loop", arr)
-            } else {
-                arr[i] = sub;
-                // console.log("chech arr in the else loop", arr)
+            if(sub == 0){
+               arr.splice(i, 1)
+               i -= 1
+            }else{
+                arr[i] = sub
             }
-        //  console.log("check i here after if statement ", i)
-        
-         }
+            console.log("check i", i)
+        }
     }
-    return output;
+    return sizeOfArray
 }
