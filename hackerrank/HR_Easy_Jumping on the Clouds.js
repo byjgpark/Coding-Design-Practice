ce8cc21a017f4cdfa268d1eb6106990f19005dda
Jungi Park <byjungy93@gmail.com>
2023-05-13 12:41:51 +0900

    [Working] Easy_Jumping on the Clouds_d2

--- hackerrank/HR_Easy_Jumping on the Clouds.js
+++ hackerrank/HR_Easy_Jumping on the Clouds.js
@@ -1,5 +1,19 @@
 function jumpingOnClouds(c) {
     // Write your code here
-    
-    console.log("check c ", c)
+    console.log("check c", c)
+    let jump = 0;
+     for(let i=0;i<c.length;i++){
+         if(c[i]==0 && c[i+2]==0){
+             console.log("first condition ", c[i],"&", c[i+2])
+             jump+=1;
+             i = i+1;
+         }else if(c[i]==0 && c[i+2]==1){
+             console.log("second condition ", c[i],"&", c[i+2])
+             jump+=1;
+         }else if(c[i]==0 && c[i+1]==0){
+             console.log("third condition ", c[i],"&", c[i+2])
+             jump+=1;
+         }
+     }
+     return jump;
 }
