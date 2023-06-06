58c7e05b22a8d7c3e151f7a0886a3fa95fc3c189
Jungi Park <byjungy93@gmail.com>
2023-06-06 15:55:16 +0900

    [Working] HR_Easy_Counting_Valleys_d6

--- hackerrank/HR_Easy_Counting_Valleys.js
+++ hackerrank/HR_Easy_Counting_Valleys.js
@@ -1,30 +1,19 @@
 function countingValleys(steps, path){
     // Write your code here
     
-    console.log("check steps", steps, "path", path)
+    console.log("check step", steps, "check path", path)
     
-    let strArr = path.split('')
-    
-    
-    console.log("hey check strArr", strArr)
-    
-    let count = 0
-    let result = 0
-    for(let step=0; step<steps; step++){
-        
-        console.log('check here', strArr[step].toLowerCase())
-        
-        if(count == 0 && strArr[step].toLowerCase() == 'd')
-        {
-            count -= 1
-            console.log("hey check count", count)
-            result += 1
-            console.log("check result", result)
-        }else if(strArr[step].toLowerCase() == 'd'){
-            count -= 1
-        }else {
-            count += 1
+    let valley = 0;
+    let elevation = 0;
+    for(let i =0; i < steps; i++){
+        if( path[i] == 'D'){
+            elevation--;
+        }else{
+            if(elevation == -1){
+                valley++;
+            }
+            elevation++
         }
     }
-    return result
-}xx
+    return valley;
+}
