90058d14db733dfe1747bd909fa3941fe92e60b9
Jungi Park <byjungy93@gmail.com>
2023-06-07 21:19:55 +0900

    [Working] HR_Easy_Counting_Valleys_d7

--- hackerrank/HR_Easy_Counting_Valleys.js
+++ hackerrank/HR_Easy_Counting_Valleys.js
@@ -1,18 +1,20 @@
 function countingValleys(steps, path){
     // Write your code here
     
-    console.log("check step", steps, "check path", path)
-    
     let valley = 0;
     let elevation = 0;
     for(let i =0; i < steps; i++){
         if( path[i] == 'D'){
             elevation--;
+            console.log("check elevation in D :", elevation,"check valley :", valley)
         }else{
             if(elevation == -1){
+                console.log("check elevation in elevation == -1 :", elevation)
                 valley++;
+                console.log("check valley in elevation == -1 :", valley)
             }
             elevation++
+            console.log("check elevation in U :", elevation, "valley :", valley)
         }
     }
     return valley;
