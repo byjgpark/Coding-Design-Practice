6eed51fe6f77f1d1c5ba385682ad7a4e375a909a
Jungi Park <byjungy93@gmail.com>
2023-06-19 11:38:12 +0900

    [Working] HR_Easy_ Staircase_d2

--- hackerrank/HR_Easy_Staircase.js
+++ hackerrank/HR_Easy_Staircase.js
@@ -1,20 +1,35 @@
 function staircase(n){
-
-    // N = 4
-    // SSS# i=0, S=3, #=1
-    // SS## i=1, S=2, #=2
-    // S### i=2, S=1, #=3
-    // #### i=3, S=0, #=4
-
     
-    for (let i = 0; i < n; i++){
-        let string = "";
-        for (let j = 0; j < n-i-1; j++){
-            string += " ";
+    // N = 4
+    // 0123
+    // SSS# i = 0, S = 3, #=1
+    // SS## i = 1, S = 2, #=2 
+    // S### i = 2, S = 1, #=3
+    // #### i = 3, S = 0, #=4
+    
+    // S= n-i-1
+    // #= i+1
+    
+    // Looping through n
+    for(let i = 0; i< n; i++){
+        
+        // Initialzing an empty string
+        let string = ""
+        
+        // Looping through and Assigning empty spaces times 
+        // if J is smaller than S= n-i-1 
+        for(let j = 0; j< n - i - 1; j++){
+            string += " "
         }
-        for (let k = 0; k < i+1; k++){
-            string += "#";
+        
+        // Looping through and Assigning # 
+        // if J is smaller than #= i+1
+        for(let k = 0; k< i + 1; k++ ){
+            string += "#"
         }
-        console.log(string);
+        
+        // printout string that meets the given conditions
+        console.log(string)
     }
+    
 }
