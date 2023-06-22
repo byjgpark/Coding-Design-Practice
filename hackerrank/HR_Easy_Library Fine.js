39ed9f60c40f999bb3eda17af1bd4da913e46f58
Jungi Park <byjungy93@gmail.com>
2023-06-22 11:48:17 +0900

    [Finished] HR_Easy_Library Fine_d2

--- hackerrank/HR_Easy_Library Fine.js
+++ hackerrank/HR_Easy_Library Fine.js
@@ -1,24 +1,12 @@
 function libraryFine(d1, m1, y1, d2, m2, y2) {
-    // Write your code here
-    console.log("check d1",d1,"m1",m1,"y1",y1,"d2",d2,"m2",m2,"y2",y2)
     
-    let number = 0;
     if( y1 > y2){
-    number = (y1 - y2) * (10000)
-        if( m1 > m2){
-        number = (m1 - m2) * (500)
-            if( d1 > d2){
-            number = (d1 - d2) * (15)
-                        }    
-                    }
-                }
-    else{
-         if( m1 > m2){
-        number = (m1 - m2) * (500)
-            if( d1 > d2){
-            number = (d1 - d2) * (15)
-                        }    
-                    }
+        return 10000
+    }else if( y1 == y2 && m1 > m2 ){
+        return ((m1 - m2) * 500)
+    }else if( y1 == y2 && m1 == m2 && d1 > d2){
+        return ((d1 - d2) * 15)
+    }else if( y1 <= y2 || m1 <= m2 || d1 <= d2){
+        return 0
     }
-    return number
 }
