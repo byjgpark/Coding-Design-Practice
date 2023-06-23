a49afbeaed6121c3e89e783f7481be5e98bea0c5
Jungi Park <byjungy93@gmail.com>
2023-06-23 10:30:55 +0900

    [Working] HR_Easy_TIme Conversion_d1

---
+++ hackerrank/HR_Easy_Time Conversion.js
@@ -0,0 +1,17 @@
+function timeConversion(s) {
+    // Write your code here
+    
+    console.log("check s", s)
+    
+    const time = s.slice(0,8).split(':');
+
+    arr[0] = (s.indexOf('PM') > -1) ?
+        (time[0] == 12 ? '12' : NUmber(time[0]) + 12) :
+        (time[0] == 12 ? '00' : time[0]);
+        
+    // console.log("check arr[0]", arr[0] = (s.indexOf('PM') > -1) ?
+    //     (time[0] == 12 ? '12' : Number(time[0]) + 12) :
+    //     (time[0] == 12 ? '00' : time[0]))    
+        
+    return time.join(':');
+}
