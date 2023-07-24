a43e72b59b0a3be4c237e53d86565b09068fb5d0
Jungi Park <byjungy93@gmail.com>
2023-07-24 21:33:47 +0900

    [Working] HR_Easy_Apple and Orang_d9

--- hackerrank/HR_Easy_Apple and Orange.js
+++ hackerrank/HR_Easy_Apple and Orange.js
@@ -1,12 +1,36 @@
- 
- function countApplesAndOranges(s, t, a, b, apples, oranges) {
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
     // Write your code here
-
-
     
-
-
-
-
+    console.log("check s", s, "t", t, "a", a, "b", b, "apple", apples, "organes", oranges)
     
+    
+    let newApples = apples;
+    let newOranges = oranges;
+    
+    for(let i = 0; i < apples.length; i++){
+        newApples[i] += a;
+    }
+    
+    for(let j = 0; j < oranges.length; j++){
+        newOranges[j] += b;
+    }
+    
+    console.log("check newApples", newApples)
+    console.log("check newOranges", newOranges)
+    
+    let appleCount = 0;
+    let oranges
+    for (let k = s; k <= t; k++){
+        if(newApples.includes(k)){
+            appleCount++
+        }
+        
+        if(newOranges.includes(k)){
+            new
+        }
+    }
+    
+    let orangesCount = 0;
+    for(let o = s;)
+
 }
