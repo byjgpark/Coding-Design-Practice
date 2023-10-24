7a9a52eceb00f18569d2b618912e7a6b83692986
Jungi Park <byjungy93@gmail.com>
2023-10-24 21:43:41 +0900

    [Working] HR_Easy_Subarray Division_d2

--- hackerrank/HR_Subarray Division.js
+++ hackerrank/HR_Subarray Division.js
@@ -1,1 +1,27 @@
-     
+ // Write your code here
+    
+  console.log("check s", s, "d", d, "m", m)
+    
+  let count = 0;
+  let sum = 0;
+
+  for(let index = 0; index < m; index++) {
+    sum += s[index]
+  }
+
+  if (sum === d) {
+    count++;
+  }
+
+  for(let i = m; i < s.length; i++) {
+    console.log("check i :",i, "m :",m)
+    console.log("check s[i] :",s[i] , "s[i -m] :", s[i - m])
+    console.log("sum in for-loop", sum)
+    sum += s[i] - s[i - m];
+
+    if (sum === d) {
+      count++;
+    }
+  }
+
+  return count;
