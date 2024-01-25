344efc1978fd46f70c3180b8e0717d34f2623c59
Jungi Park <byjungy93@gmail.com>
2024-01-25 22:10:20 +0900

    [Working] HR_Easy_Fair Rations_D1-1

--- hackerrank/HR_Easy_Fair Rations.js
+++ hackerrank/HR_Easy_Fair Rations.js
@@ -1,16 +1,35 @@
 function fairRations(a) {
+    
     let temp = a, loaves = 0, i;
     
+    console.log("check i", i)
+    
+    console.log("check a", a);
+    
+    console.log("check temp", temp);
+    
     for (i = 0; i < temp.length - 1; i++) {
+        
+        console.log("Check temp[i]", temp[i]);
+        
+        console.log("check modules ",  temp[i] % 2);        
+        
         if (temp[i] % 2 != 0) {
             temp[i]++;
             temp[i + 1]++;
             loaves++;
         }
+        
+        console.log("check i inside of the for-loop", i)
     }
+    
+    console.log("check i after for-loop", i);
 
     if (temp[i] % 2 === 0) {
-        return loaves * 2
+        
+        console.log("check loaves : ", loaves);
+        
+        return loaves * 2;
     }
 
     temp = [];
@@ -18,10 +37,13 @@ function fairRations(a) {
     i = 0;
 
     for (i = temp.length - 1; i > 0; i--) {
+        
         if (temp[i] % 2 != 0) {
             temp[i]++;
             temp[i - 1]++;
         }
+        
+        console.log("check temp.length", temp.length);
     }
 
     if (temp[i] % 2 === 0) {
