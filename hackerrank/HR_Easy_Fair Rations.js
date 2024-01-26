a49d32094331434e12bb5c5e425d62984af911cb
Jungi Park <byjungy93@gmail.com>
2024-01-27 08:51:16 +0900

    [Finished] HR_Easy_Fair Rations_D2

--- hackerrank/HR_Easy_Fair Rations.js
+++ hackerrank/HR_Easy_Fair Rations.js
@@ -1,54 +1,19 @@
 function fairRations(a) {
     
-    let temp = a, loaves = 0, i;
-    
-    console.log("check i", i)
-    
-    console.log("check a", a);
-    
-    console.log("check temp", temp);
-    
-    for (i = 0; i < temp.length - 1; i++) {
+    let loave = 0, i, temp = a;
+
+    for(i = 0; i < temp.length - 1 ; i++){
         
-        console.log("Check temp[i]", temp[i]);
-        
-        console.log("check modules ",  temp[i] % 2);        
-        
-        if (temp[i] % 2 != 0) {
-            temp[i]++;
-            temp[i + 1]++;
-            loaves++;
+        if(temp[i] % 2 != 0){
+            temp[i]++
+            temp[i+1]++
+            loave++;
         }
-        
-        console.log("check i inside of the for-loop", i)
+    }
+
+    if ( temp[i] % 2 == 0){
+        return loave * 2;
     }
     
-    console.log("check i after for-loop", i);
-
-    if (temp[i] % 2 === 0) {
-        
-        console.log("check loaves : ", loaves);
-        
-        return loaves * 2;
-    }
-
-    temp = [];
-    loaves = 0;
-    i = 0;
-
-    for (i = temp.length - 1; i > 0; i--) {
-        
-        if (temp[i] % 2 != 0) {
-            temp[i]++;
-            temp[i - 1]++;
-        }
-        
-        console.log("check temp.length", temp.length);
-    }
-
-    if (temp[i] % 2 === 0) {
-        return loaves * 2;
-    }
-
-    return 'NO';
+    return "NO"
 }
