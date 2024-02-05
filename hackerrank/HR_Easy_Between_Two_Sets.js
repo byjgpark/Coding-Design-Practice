9dbdeb6c1851584d36aa16b0fc7d0b72eeaeb611
Jungi Park <byjungy93@gmail.com>
2024-02-05 12:49:28 +0900

    [Working] HR_Easy_Between_Two_Sets_D6

--- hackerrank/HR_Easy_Between_Two_Sets.js
+++ hackerrank/HR_Easy_Between_Two_Sets.js
@@ -1,2 +1,27 @@
- 
+function getTotalX(a, b) {
+    // Write your code here
+    console.log("Check here gcd : ", gcd(75,25));
+    
+    console.log("Check here lcm : ", lcm(75,25));
+    
+    console.log("check % = ", 75%25)
+}
+
+function gcd(x, y) {
+    
+  x = Math.abs(x);
+  y = Math.abs(y);
+  
+  while(y) {
+    var t = y;
+    y = x % y;
+    x = t;
+  }
+  return x;
+}
+
+function lcm(x, y){
+    return (x * y) / gcd(x, y); 
+}
+
  // starts to understand the question day2 
