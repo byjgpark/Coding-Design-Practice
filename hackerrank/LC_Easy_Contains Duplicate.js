87ae6959cf8cb9cf2ff8f954aa6679ee7f4e86df
Jungi Park <byjungy93@gmail.com>
2024-02-19 12:27:30 +0900

    [Finished] LC_Easy_Contains Duplicate_D2

--- hackerrank/LC_Easy_Contains Duplicate.js
+++ hackerrank/LC_Easy_Contains Duplicate.js
@@ -1,1 +1,23 @@
 
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    
+    const a = new Set(nums)
+
+    const b = [...a]
+
+    console.log("check nums", nums);
+    console.log("check a", a.length);
+    console.log("check b", b.length);
+
+    console.log("check b.lenth == a.length :", b.length === a.length);
+
+    if( b.length === nums.length){
+        return false
+    }else{
+        return true
+    }
+};
