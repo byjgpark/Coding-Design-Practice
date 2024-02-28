669f24204896b55feb613b568565ea07a61d468b
Jungi Park <byjungy93@gmail.com>
2024-02-28 16:51:14 +0900

    [Working] LC_Two_Sum_D2

--- LeetCode/LC_Easy_Valid Anagram.py
+++ LeetCode/LC_Easy_Valid Anagram.py
@@ -8,6 +8,7 @@ class Solution:
 
         print("check len(s)",len(s))
         
+        
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
