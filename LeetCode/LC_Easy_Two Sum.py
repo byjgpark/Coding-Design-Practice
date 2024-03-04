c2039c3bb4ff1a1703879ffcd9f81225de08d0a3
Jungi Park <byjungy93@gmail.com>
2024-03-04 12:39:17 +0900

    [Finished] LC_Two_Sum_D3

--- LeetCode/LC_Easy_Two Sum.py
+++ LeetCode/LC_Easy_Two Sum.py
@@ -1,1 +1,17 @@
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        hashmap = {}
+
+        for i in range(len(nums)):
+        
+            complement = target - nums[i]
+            
+            # print("check hashmap", hashmap)
+
+            # print("check complment", complement)    
+
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[nums[i]] = i 
