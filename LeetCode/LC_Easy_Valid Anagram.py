4dfb23f12a15d640f54a666ae4c6ef140b34878f
Jungi Park <byjungy93@gmail.com>
2024-03-06 22:53:36 +0900

    [Working] LC_Easy_Valid Palindrome_D1

--- LeetCode/LC_Easy_Valid Anagram.py
+++ LeetCode/LC_Easy_Valid Anagram.py
@@ -1,14 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
+
         if len(s) != len(t):
             return False
 
         countS, countT = {}, {}
 
-        print("check len(s)",len(s))
-        
-        
+        print("check len(s)", len(s))
+
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
@@ -16,5 +15,3 @@ class Solution:
 
         return countS == countT
         # print("countT[s[i]]", i)
-
-
