b3ed471ca3e686663d0b31c99392af801bceecb1
Jungi Park <byjungy93@gmail.com>
2024-03-09 19:21:53 +0900

    [Finished] LC_Easy_Valid Palindrome_D3

--- LeetCode/LC_Easy_Valid Palindrome.py
+++ LeetCode/LC_Easy_Valid Palindrome.py
@@ -2,12 +2,25 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
 
         L = 0
-        R = len(s)
+        R = len(s) - 1
 
-        while( L < R):
+        while L < R:
+            print("check L :", L, "check R :",R)
+            print("check s[L].lower() :",s[L].lower(), "s[R].lower()", s[R].lower())
 
-            # print("check",s[L])
-            # L+=1
+            if not s[L].isalnum():
+                L+= 1
+                continue
 
-            if( not s[L].isalnum()):
-                print("s[L]", s[L])
+            if not s[R].isalnum():
+                R-= 1
+                continue
+
+      
+            if s[L].lower() != s[R].lower():
+                return False
+
+            L+=1
+            R-=1
+        
+        return True
