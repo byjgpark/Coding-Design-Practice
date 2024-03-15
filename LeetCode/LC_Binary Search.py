20663e423abacd102615eb013c8381434d50f780
Jungi Park <byjungy93@gmail.com>
2024-03-15 12:38:30 +0900

    [Finished ] LC_Easy_Binary Search_D3

--- LeetCode/LC_Binary Search.py
+++ LeetCode/LC_Binary Search.py
@@ -1,26 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
-        l, r = 0, len(nums) - 1
+        L = 0
+        R = len(nums) - 1
 
+        while L <= R:
+            
+            m = L + ( (R - L) // 2 )
 
-        print("check l", l, "check r", r)
-
-        print("check 1/2", 1//2)
-
-        while l <= r:
-            m = l + ((r - l) // 2)  # (l + r) // 2 can lead to overflow
-            # m = (l + r) // 2
-            print("2nd check m", m)
-            # if nums[2] = 3 > 0
-            if nums[m] > target:
-                # l = 0, r = 5, m = 2
-                r = m - 1
-                # r = 1
-            elif nums[m] < target:
-                l = m + 1
-            else:
+            if nums[m] < target:
+                L = m + 1
+            elif nums[m] > target:
+                R = m - 1
+            else: 
                 return m
         return -1
+            
+        
 
         
