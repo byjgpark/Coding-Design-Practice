d6aee0b436d89b4aea1983131ceb1af6b0084c8d
Jungi Park <byjungy93@gmail.com>
2024-03-19 14:58:18 +0900

    [Finished] LC_Maximum Average Subarray_D5

--- LeetCode/LC_Maximum Average Subarray.py
+++ LeetCode/LC_Maximum Average Subarray.py
@@ -1,41 +1,37 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
 
-        # 1, find max num til k
-        # 2, sliding windiw -> subtracking left pointer and adding right pointer from the sum
-        # 3, find the max => find average 
-          # Get sum for starting window
-        _sum = sum(nums[:k])
-        max_sum = _sum
+        L = 0
+        R = k
 
-        # Start sliding window
-        start_index = 0
-        end_index = k
+        sumNum = 0
 
-        print("check end_index", end_index)
-        print("check len(nums)", len(nums))
-        while end_index < len(nums):
+        for i in range(k):
+            sumNum +=nums[i]
 
-            print("check end_index", end_index)
-            print("check len(nums)", len(nums))
+        newSum = sumNum
+
+        while R < len(nums):
+            newSum -= nums[L]
+            L+=1
+            newSum += nums[R]
+            R+=1
+
+            sumNum = max(newSum, sumNum)
+        
+        return sumNum/ k
             
-            _sum -= nums[start_index]  # Remove previous element
-            start_index += 1
 
-            _sum += nums[end_index]  # Add next element
-            end_index += 1
-
-            print("Check _sum", _sum)
-            print("Check max_sum", max_sum)
-            max_sum = max(max_sum, _sum)  # Update max sum
-
-        # Return the average
-        return max_sum / k
         
 
+            
 
-           
-            # break
+
+    
+
+
+
+
             
             
 
