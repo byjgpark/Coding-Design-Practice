731e9bfbfd627b776f0a1a134085bc82c6288fce
Jungi Park <byjungy93@gmail.com>
2024-03-21 12:45:28 +0900

    [Finished] LC_Best Time to Buy and Sell Stock_D2

--- LeetCode/LC_Best Time to Buy and Sell Stock.py
+++ LeetCode/LC_Best Time to Buy and Sell Stock.py
@@ -1,15 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1
-        maxP = 0
+        l = 0
+        r = 0
+        maxNum = 0
 
         while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                maxP = max(maxP, profit)
+
+            sumNum = prices[r] - prices[l]
+            if sumNum > 0:
+                maxNum = max(sumNum,maxNum)
             else:
                 l = r
-            r += 1
-        return maxP
+            r+=1
 
-
+        return maxNum
