c83ee3da5de72179655f49cc54c290cca6a9af52
Jungi Park <byjungy93@gmail.com>
2024-03-28 22:18:37 +0900

    [Working] Singly LinkedList_D3

--- LeetCode/SinglyLinkedList.py
+++ LeetCode/SinglyLinkedList.py
@@ -1,18 +1,31 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
 class Node:
-    def __init__(self, data):
+    def __init__(self, data=None):
         self.data = data
         self.next = None
+    def __str__(self):
+        return f"self.data : {self.data} | self.next : {self.next}"
 
 class LinkedList:
     def __init__(self):
         self.head = None
+        
+    def __str__(self):
+        return f"self.head : {self.head}"
 
     def append(self, data):
+        print("check data", data)
+        print("check self.head", self.head)
         new_node = Node(data)
-        if self.head is None:
+        # print("check new_node :", new_node)
+        if not self.head:
+            # print("check not self.head", self.head)
             self.head = new_node
             return
         last_node = self.head
+    
+        print("check last_node.next", last_node.next)
         while last_node.next:
             last_node = last_node.next
         last_node.next = new_node
@@ -22,20 +35,20 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
-    def delete(self, data):
-        if self.head is None:
-            return
-
-        if self.head.data == data:
-            self.head = self.head.next
-            return
-
+    def delete_node(self, data):
         current_node = self.head
-        while current_node.next:
-            if current_node.next.data == data:
-                current_node.next = current_node.next.next
-                return
+        if current_node and current_node.data == data:
+            self.head = current_node.next
+            current_node = None
+            return
+        prev_node = None
+        while current_node and current_node.data != data:
+            prev_node = current_node
             current_node = current_node.next
+        if current_node is None:
+            return
+        prev_node.next = current_node.next
+        current_node = None
 
     def display(self):
         current_node = self.head
@@ -45,12 +58,12 @@ class LinkedList:
         print("None")
 
 # Example usage:
-linked_list = LinkedList()
-linked_list.append(1)
-linked_list.append(2)
-linked_list.append(3)
-linked_list.prepend(0)
-
-linked_list.display()  # Output: 0 -> 1 -> 2 -> 3 -> None
-linked_list.delete(2)
-linked_list.display()  # Output: 0 -> 1 -> 3 -> None
+if __name__ == "__main__":
+    linked_list = LinkedList()
+    linked_list.append(1)
+    linked_list.append(2)
+    linked_list.append(3)
+    # linked_list.prepend(0)
+    linked_list.display()  # Output: 0 -> 1 -> 2 -> 3 -> None
+    # linked_list.delete_node(2)
+    # linked_list.display()  # Output: 0 -> 1 -> 3 -> None
