ea62bfcb0f4c7f3590ec03a1e27d42c859af7f8a
Jungi Park <byjungy93@gmail.com>
2024-04-05 12:58:31 +0900

    [Working] Singly LinkedList_D7

--- LeetCode/SinglyLinkedList.py
+++ LeetCode/SinglyLinkedList.py
@@ -1,78 +1,64 @@
 # Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
 class Node:
+     # Constructor method
     def __init__(self, data):
         self.data = data
         self.next = None
-        
+    
     def __str__(self):
-        return f"self.data : {self.data} | self.next : {self.next}"
+        return f"Check self.data : {self.data} || self.next : {self.next}"    
 
 class LinkedList:
+    
     def __init__(self):
         self.head = None
-        
-    # def __str__(self):
-    #     return f"self.head : {self.head}"
 
     def append(self, data):
-        # print("check data", data)
-        print("check self.head", self.head)
-        new_node = Node(data)
-        # print("check new_node :", new_node)
+        newNode = Node(data)
+        
         if not self.head:
-            print("check not self.head", self.head)
-            self.head = new_node
+            self.head = newNode
             return
         current = self.head
         
-        print("check last_node", current, "&&& last_node.next", current.next)
-        
         while current.next:
-            print("Check self.head.next inside of while-loop :", current.next)
-            # print("Check self.head inside of while-loop :", self.head)
-            print("Check last_node inside of while-loop :", current)
             current = current.next
-            print("Check self.head at the end of each iteration inside of  while-loop :", self.head)
-            print("Check last_node at the end of each iteration inside of  while-loop :", current)
-        current.next = new_node
-        print("check self.head at the end of function :", self.head)
-
-    def prepend(self, data):
-        new_node = Node(data)
-        new_node.next = self.head
-        self.head = new_node
-
-    def delete_node(self, data):
-        current_node = self.head
-        if current_node and current_node.data == data:
-            self.head = current_node.next
-            current_node = None
-            return
-        prev_node = None
-        while current_node and current_node.data != data:
-            prev_node = current_node
-            current_node = current_node.next
-        if current_node is None:
-            return
-        prev_node.next = current_node.next
-        current_node = None
-
+        current.next = newNode 
+        
     def display(self):
-        # print("check display self.head", self.head)
-        current_node = self.head
-        while current_node:
-            print(current_node.data, end=" -> ")
-            current_node = current_node.next
-        print("None")
-
-# Example usage:
+        current = self.head
+        while current:
+            print(current.data, end =" -> ")
+            current = current.next
+        print("end")
+        
+    def prepend(self, data):
+        newNode = Node(data)
+        
+        current = self.head
+    
+        self.head = newNode
+        
+        self.head.next = current
+        
+    def delete(self, data):
+        current = self.head
+        # while current.data == data: 
+        print("check current.data", current.data)
+        
+        
+        
 if __name__ == "__main__":
-    linked_list = LinkedList()
-    linked_list.append(1)
-    linked_list.append(2)
-    linked_list.append(3)
-    # linked_list.prepend(0)
-    # linked_list.display()  # Output: 0 -> 1 -> 2 -> 3 -> None
-    # linked_list.delete_node(2)
-    # linked_list.display()  # Output: 0 -> 1 -> 3 -> None
+    
+    linkedList = LinkedList()
+    linkedList.append(1)
+    linkedList.append(2)
+    linkedList.append(3)
+    linkedList.prepend(0)
+    linkedList.delete(0)
+    # linkedList.display()
+    
+    
+            
+    
