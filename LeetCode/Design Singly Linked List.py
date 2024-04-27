d2203bb1d4ca4876e49b52d44d3f74ae27e2dae2
Jungi Park <byjungy93@gmail.com>
2024-04-27 09:40:54 +0900

    [Working] Design Singly Linked List_D16

--- LeetCode/Design Singly Linked List.py
+++ LeetCode/Design Singly Linked List.py
@@ -15,47 +15,58 @@ class LinkedList:
     # def get(self, index: int) -> int:
         
     def insertHead(self, val: int) -> None:
-        # print("check self.data", self.head)
-        # new_node = Node(val)
-        # print("check new_node", new_node)
-        # curr = self.head.next
-        # curr = new_node
-        # print("check curr", curr)
-        # self.head.next = new_node
-        # # self.head.next = curr
-        # print("check self.head.next", self.head.next)
         
         new_node = Node(val)
-        
-        print("check new_node : ", new_node)
         new_node.next = self.head.next
-        print("check new_node : ", new_node)
         self.head.next = new_node
-        # new_node.next = self.head.next
-        # self.head.next = new_node
+        
+        if not new_node.next:
+            self.tail = self.head.next
+
+    def insertTail(self, val: int) -> None:
+        self.tail.next = Node(val)
+        self.tail = self.tail.next
+        
+    def remove(self, index: int) -> bool:
+        
+        curr = self.head
+        i = 0
+        while i < index:
+            curr = curr.next
+            i+=1
         
         print("check self.head", self.head)
-        print("------------------------------------------")
+        print("check curr", curr)
         
+        self.head.next = curr.next
         
-
-    # def insertTail(self, val: int) -> None:
-        
-
-    # def remove(self, index: int) -> bool:
-        
-
-    # def getValues(self) -> List[int]:
+        print("after check self.head", self.head)
+        # while curr:
+            # print("hey check here")
+            
+            
         
 
-       
+    def getValues(self) -> list[int]:
+        # print("check self.head", self.head)
+        curr = self.head
+        # print("cherck curr", curr)
+        arr = []
+        while curr and curr.next:
+            arr.append(curr.next.data)
+            curr = curr.next
+            # print("Check curr in the while-loop", curr)
+        return arr
+    
 if __name__ == "__main__":
     linkedList = LinkedList()
     linkedList.insertHead(3)
     linkedList.insertHead(1)
+    linkedList.insertHead(4)
+    linkedList.insertTail(9)
     # linkedList.insertHead(0)
     # linkedList.insertTail(9)
     # linkedList.insertTail(4)
     # linkedList.insertTail(2)
-    # linkedList.remove(3)
-    # print("Get Values : ", linkedList.getValues())
+    linkedList.remove(2)
+    print("Get Values : ", linkedList.getValues())
