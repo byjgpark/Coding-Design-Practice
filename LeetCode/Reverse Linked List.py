e4598ca38090c69e5855be8d58d1343a679058d1
Jungi Park <byjungy93@gmail.com>
2024-05-04 09:19:24 +0900

    [Working] Reverse Linked List_D2

--- LeetCode/Reverse Linked List.py
+++ LeetCode/Reverse Linked List.py
@@ -1,8 +1,27 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        print("Check head", head)
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+
+        print("check prev =", prev)
+        print("check curr =", curr)
+
+        while curr:
+            temp = curr.next
+            print("check 1st temp in the while-loop", temp)
+            curr.next = prev
+            print("check curr.next in the while-loop", curr.next)
+            prev = curr
+            print("check prev in the while-loop", prev)
+            curr = temp
+            print("check curr at the end of while-loop", curr)
+
+            print("------------------------------------------------------------------------------------------------------")
+        # print("check prev outside of while-loop", prev)
+        return prev
