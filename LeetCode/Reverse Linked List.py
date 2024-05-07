b7beed94e32203f1ff7f4daa1aa3bbab71407ed2
Jungi Park <byjungy93@gmail.com>
2024-05-07 12:21:03 +0900

    [Finished] Reverse Linked List_D5

--- LeetCode/Reverse Linked List.py
+++ LeetCode/Reverse Linked List.py
@@ -1,31 +1,25 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        prev, curr = None, head
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        print("check prev outside of while :", prev)
-        print("check curr outside of while :", curr)
+        prev, curr = None, head 
+
+        print("check prev", prev)
+
+        print("check curr", curr)
 
         while curr:
-            print("check 1st prev in the beginning :", prev)
-            print("check 1st curr in the beginning :", curr)
-            print("check 1st curr.next in the beginning :", curr.next)
+
             temp = curr.next
-            print("check 1st temp in the while-loop :", temp)
+
             curr.next = prev
-            print("check 2nd curr in the while-loop :", curr)
-            # print("check curr.next in the while-loop :", curr.next)
+
             prev = curr
-            print("check 2nd prev in the while-loop :", prev)
+
             curr = temp
-            print("check 2nd temp at the end of while-loop :", temp)
-            print("check 3rd curr at the end of while-loop :", curr)
-            print("------------------------------------------------------------------------------------------------------")
-        # print("check prev outside of while-loop", prev)
+
         return prev
