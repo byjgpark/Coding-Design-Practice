99ea80822351a94020fb4f0e4412b2e62a273a2a
Jungi Park <byjungy93@gmail.com>
2024-05-08 12:58:14 +0900

    [Working] Merge Two Sorted Lists_D2

--- LeetCode/LC_Merge Two Sorted Lists.py
+++ LeetCode/LC_Merge Two Sorted Lists.py
@@ -10,20 +10,27 @@ class Solution:
 
         dummy = node = ListNode()
 
-        print("check dummy :", dummy)
-        print("check node :", node)
+        print("check dummy =", dummy)
+        print("check node =", node)
 
         while list1 and list2:
-            print("check list1.val", list1.val)
-            print("check list2.val", list2.val)
+            print("check list1.val =", list1.val)
+            print("check list2.val =", list2.val)
+            print("check list1 =", list1)
             if list1.val < list2.val:
                 node.next = list1
                 list1 = list1.next
+                print("check node inside of if-statement :",node)
+                print("check list1 inside of while-loop :",list1)
             else:
                 node.next = list2
                 list2 = list2.next
+                print("check node inside of else-statement :",node)
+                print("check list2 inside of while-loop :",list2)
+            # print("Check node inside of while-loop =", node)
             node = node.next
-
+ 
         node.next = list1 or list2
-
+        print("Check node =", node)
+        print("Check dummy =", dummy)
         return dummy.next
