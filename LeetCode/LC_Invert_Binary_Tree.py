d7bd281a10b1c7784d8b729f26c0db59742fdc77
Jungi Park <byjungy93@gmail.com>
2024-06-04 12:58:20 +0900

    [Working] Invert_Binary_Tree_D1

---
+++ LeetCode/LC_Invert_Binary_Tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        # swap the children
+        # root.left, root.right = root.right, root.left
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+        
+        # make 2 recursive calls
+        self.invertTree(root.left)
+        print("check root.right", root)
+        self.invertTree(root.right)
+        
+        print("check root at the end of function :", root)
+        
+        return root
