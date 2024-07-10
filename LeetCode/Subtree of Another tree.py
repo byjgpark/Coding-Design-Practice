2e705706724401f729a6156365a126018ba142c2
Jungi Park <byjungy93@gmail.com>
2024-07-10 21:37:16 +0900

    [Finished] Subtree of Another tree_D3

--- LeetCode/Subtree of Another tree.py
+++ LeetCode/Subtree of Another tree.py
@@ -4,10 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-class Solution:   
+class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         
+        if not subRoot:
+            return True
+        if not root: 
+            return False
+        
+
+        if self.sameTree(root, subRoot):
+            return True
+        return ( self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot) )
     
-    
-    
+
+
+    def sameTree(self, lTree, rTree) -> bool:
+
+        if not lTree and not rTree:
+            return True 
+        if lTree and rTree and lTree.val == rTree.val:
+            return ( self.sameTree(lTree.left, rTree.left) and self.sameTree(lTree.right,rTree.right))
+        return False
