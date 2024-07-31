6b361268d9874ea40f1f40dec2c2d412f336c403
Jungi Park <byjungy93@gmail.com>
2024-07-31 12:56:12 +0900

    [Working] Binary_Heap_D10

--- LeetCode/BinaryHeap (4).py
+++ LeetCode/BinaryHeap (4).py
@@ -1,94 +1,73 @@
-class BinaryHeap:
+class Heap: 
     def __init__(self):
         self.heap = []
-
-    def is_empty(self):
-        return len(self.heap) == 0
-
+        
     def insert(self, key):
         self.heap.append(key)
-        self._heapify_up(len(self.heap) - 1)
-
-    def _heapify_up(self, index):
-        parent_index = (index - 1) // 2
+        self.heaplify_up(len(self.heap)-1)
         
-        # print("check index ", index, "something ", (6-1)// 2)
-        print("check index ", index, "check parent_index", parent_index)
+    def heaplify_up(self, index):
+        parent_index = (index - 1) // 2 
         
         while parent_index >= 0 and self.heap[parent_index] > self.heap[index]:
             self.heap[parent_index], self.heap[index] = self.heap[index], self.heap[parent_index]
             index = parent_index
+            parent_index = (index -1) // 2
+    
+    def is_empty(self):
+        if not self.heap:
+            return 
         
-            parent_index = (index - 1) // 2
-          
-    def get_min(self):
+    def get_minValue(self):
         if self.is_empty():
-            return None
-        return self.heap[0]
-
-    def delete_min(self):
+            return 
+        return self.heap[0] 
+    
+    def remove_minValue(self):
         if self.is_empty():
-            return None
+            return 
         
-        # find min value 
-        min_value = self.heap[0]
-        
-        # assign the last index at the first index
+        #1 remove the min element
+        #2 swap the last element with the first element 
+        #3 bubbling down the element
+        last_value = self.heap[-1]
         self.heap[0] = self.heap[-1]
-        # pop the duplicate value
-        self.heap.pop()
-        # heapify down the min value from the tree 
-        self._heapify_down(0)
+        self.heaplify_down(0)
+        
+        
+        print("find last index ", self.heap[-1])
+        
+    def heaplify_down(self, index):
+        
+        parent_index = (index - 1) // 2
+        left_index = (parent_index * 2) + 1
+        right_index = (parent_index * 2) + 2
+        
+        print("check here len :", len(self.heap))
 
-        return min_value
+        if len(self.heap) >= index and self.heap[left_index] < self.heap[index]:
+            self.heap[index], self.heap[left_index] = self.heap[left_index], self.heap[index]
+            # index = 
+              
+            
 
-    def _heapify_down(self, index):
-        left_child_index = 2 * index + 1
-        right_child_index = 2 * index + 2
-        smallest = index
-
-        if left_child_index < len(self.heap) and self.heap[left_child_index] < self.heap[smallest]:
-            smallest = left_child_index
-
-        if right_child_index < len(self.heap) and self.heap[right_child_index] < self.heap[smallest]:
-            smallest = right_child_index
-
-        if smallest != index:
-            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
-            self._heapify_down(smallest)
-
-
-# Example Usage
-if __name__ == "__main__":
+if __name__=="__main__": 
+    heap = Heap()
     
-    heap = BinaryHeap()
-
-    # Insert elements
-    # heap.insert(3)
-    # heap.insert(2)
-    # heap.insert(15)
-    # heap.insert(5)
-    # heap.insert(4)
-    # heap.insert(45)
-    
-    # Insert elements
-    heap.insert(4)
-    heap.insert(50)
-    heap.insert(7)
-    heap.insert(55)
-    heap.insert(90)
-    heap.insert(87)
+    # heap.is_empty()
+    heap.insert(3)
     heap.insert(2)
-    
-    print("check init heap", heap.heap)
+    heap.insert(15)
+    heap.insert(5)
+    heap.insert(4)
+    heap.insert(45)
 
-    print("Minimum element in the heap:", heap.get_min())
-
-    # Delete minimum element
-    print("Deleted minimum element:", heap.delete_min())
+    print("check self.heap", heap.heap)
     
-    print("after deleting heap.heap: ", heap.heap)
-    # arr = [1,2,3,4]
+    print("check heaplify_down ", heap.heaplify_down(0))
     
-    # arr.pop()
-
+    print("get min_value", heap.get_minValue())
+    
+    print("get min_value", heap.remove_minValue())
+    
+    # print("hey check here")
