a46017383946762c801bc0b9165a89b3ae82cf12
Jungi Park <byjungy93@gmail.com>
2024-08-04 09:01:24 +0900

    [Working] Kth Largest Element in a Stream_D4

--- LeetCode/LC_Kth Largest Element in a Stream.py
+++ LeetCode/LC_Kth Largest Element in a Stream.py
@@ -1,13 +1,16 @@
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.k = k
-        self.nums = nums
+        
+        
+        
         
 
     def add(self, val: int) -> int:
 
         print("check sort", self.nums)
+        
+        
         
 
         print("check nums ", sorted(self.nums))
