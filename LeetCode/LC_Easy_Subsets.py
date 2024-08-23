5c54dcde705956cd2c884620a0120a0645e30391
Jungi Park <byjungy93@gmail.com>
2024-08-23 13:30:48 +0900

    [Working] LC_Easy_Subsets_D6

--- LeetCode/LC_Easy_Subsets.py
+++ LeetCode/LC_Easy_Subsets.py
@@ -1,15 +1,10 @@
 
-from typing import List
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
 
         subset = []
         
-        print("chek")
-        
-        print("hi I need to check")
 
         def dfs(i):
             if i >= len(nums):
@@ -21,12 +16,12 @@ class Solution:
             ## dfs(1), dfs(2)
             dfs(i + 1) 
             
-            print("returned to here 1")
+        
             # decision NOT to include nums[i]
             subset.pop()
             ## dfs(3)
             dfs(i + 1) 
-            print("returned to here 1")
+        
 
 
         dfs(0)
