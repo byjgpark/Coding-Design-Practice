ee9680bc2c485fbca38005a8c162e9ce3afcdbba
Jungi Park <byjungy93@gmail.com>
2024-08-27 14:11:12 +0900

    [Working] LC_Easy_Subsets_D7

--- LeetCode/LC_Easy_Subsets.py
+++ LeetCode/LC_Easy_Subsets.py
@@ -1,11 +1,10 @@
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        
         res = []
 
         subset = []
         
-
         def dfs(i):
             if i >= len(nums):
                 res.append(subset.copy())
@@ -13,17 +12,15 @@ class Solution:
             # decision to include nums[i]
             subset.append(nums[i])
             
-            ## dfs(1), dfs(2)
+            ## dfs(1)
             dfs(i + 1) 
-            
         
             # decision NOT to include nums[i]
             subset.pop()
-            ## dfs(3)
+    
             dfs(i + 1) 
-        
 
-
+        ## dfs(0)
         dfs(0)
         return res
     
