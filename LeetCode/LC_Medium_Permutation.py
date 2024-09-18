f8a81d29692b3bb46bf48f327dc8ec7d433385c3
Jungi Park <byjungy93@gmail.com>
2024-09-18 14:43:12 +0900

    [Working] LC_Medium_Permutation_D2

--- LeetCode/LC_Medium_Permutation.py
+++ LeetCode/LC_Medium_Permutation.py
@@ -1,1 +1,28 @@
- 
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        # base case
+        if len(nums) == 1:
+            return [nums[:]]  # nums[:] is a deep copy
+
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+            
+            print("Check perms: ", perms)
+
+            for perm in perms:
+                perm.append(n)
+            res.extend(perms)
+            nums.append(n)
+        return res
+    
+if __name__ == "__main__":
+    s = Solution()
+    nums = [1, 2, 3]
+    s.permute(nums)
+    # print(s.perm)
