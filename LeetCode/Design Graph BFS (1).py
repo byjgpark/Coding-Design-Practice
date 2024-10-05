f6392d274714336537690071072532c924cac5be
Jungi Park <byjungy93@gmail.com>
2024-10-05 11:52:51 +0900

    [Working] Design Graph BFS_D6

---
+++ LeetCode/Design Graph BFS (1).py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def BFS(graph, start):
+    visited = set()  # Keep track of the nodes that we've visited
+    queue = deque([start])  # Use a queue to implement the BFS
+
+    while queue:
+        node = queue.popleft()  # Dequeue a node from front of queue
+        if node not in visited:
+            visited.add(node)  # Mark the node as visited
+            print(node, end=' ')  # Visit the node (print its value in this case)
+            queue.extend(graph[node])  # Enqueue all neighbours 
+
+# Use the BFS function on a graph
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E'],
+}
+
+BFS(graph, 'A')  # Output: A B C D E F
+
