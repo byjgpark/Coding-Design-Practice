44c76e7251d1128ef1bd4ccb720f3b1414c4394c
Jungi Park <byjungy93@gmail.com>
2024-10-13 09:36:45 +0900

    [Working] LC_Medium_Number of Islands_D3

---
+++ LeetCode/LC_Medium_Number of Islands (1).py
@@ -0,0 +1,38 @@
+from collections import deque
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        row, column = len(grid), len(grid[0])
+
+        queue = deque()
+        visited = set()
+        island = 0
+
+        # def bfs(column):
+
+        #     while queue:
+
+            
+
+
+        print("check ", row)
+
+        # for r in row:
+        #     print("check r" r)
+            # for c in column:
+                
+
+
+        #         if column == 1 and column not in visited:
+        #             bfs(column)
+        #             island+= 1
+
+
+
+        # return island
+                    
+
+                        
+
+        
