b3dcbd1bce93f71e30ad7f8f64c25bc45af71c0f
Jungi Park <byjungy93@gmail.com>
2024-11-09 09:58:00 +0900

    [Working] LC_Pacific Atlantic Water Flow_D4

--- LeetCode/LC_Medium_Pacific Atlantic Water Flow.py
+++ LeetCode/LC_Medium_Pacific Atlantic Water Flow.py
@@ -3,6 +3,7 @@ from typing import List
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
+        
         print(f"\nGrid Dimensions: {ROWS}x{COLS}")
         print("Initial Heights Grid:")
         for row in heights:
@@ -85,3 +86,5 @@ class Solution:
 if __name__ == "__main__":
     sol = Solution()
     print(sol.pacificAtlantic([[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]))
+    
+    
