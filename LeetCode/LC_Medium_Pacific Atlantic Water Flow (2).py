9c71b4bac302f1f2bc9418226f931f03afd35d77
Jungi Park <byjungy93@gmail.com>
2024-11-15 12:18:01 +0900

    [Finished] LC_Pacific Atlantic Water Flow_D10

--- LeetCode/LC_Medium_Pacific Atlantic Water Flow (2).py
+++ LeetCode/LC_Medium_Pacific Atlantic Water Flow (2).py
@@ -3,47 +3,46 @@ class Solution:
         
         row, col = len(heights), len(heights[0])
 
-        print("check here row :", row, "col :", col)
+        print("check row :", row, "column :", col)
 
-        atl, pac = set(), set()
+        pac, atl = set(), set()
 
-        def dfs(r,c, visited, previValue):
+        def dfs(r, c, visit, previValue):
 
-            if (r < 0 or c < 0) or ( r not in range(row) or c not in range(col)) or ((r,c) in visited) or ( heights[r][c] < previValue):
-                return  
+            if (r < 0 or c< 0) or ( (r,c) in visit) or ( r not in range(row) or c not in range(col)) or ( heights[r][c] < previValue):
+                return
+            
+            print("check (r,c)", (r,c))
 
-            visited.add((r,c))
+            visit.add((r,c))
 
-            # print("Check (r,c)", (r,c))
-            # print("check previValue :", previValue)
-            dfs(r+1, c, visited, heights[r][c])
-            dfs(r-1, c, visited, heights[r][c])
-            dfs(r, c+1, visited, heights[r][c])
-            dfs(r, c-1, visited, heights[r][c])
+            dfs(r+1, c, visit, heights[r][c])
+            dfs(r-1, c, visit, heights[r][c])
+            dfs(r, c+1, visit, heights[r][c])
+            dfs(r, c-1, visit, heights[r][c])
 
         # column
         for c in range(col):
-            dfs(0, c, pac, heights[0][c])
-            dfs(row-1, c, atl, heights[row-1][c])
 
+            # print("check c", c)
+
+            dfs(0, c, pac, heights[0][c])
+            dfs(row-1,c, atl, heights[row-1][c])
+        
         # row
         for r in range(row):
-
-            print("check r in the loop", r)
             dfs(r, 0, pac, heights[r][0])
             dfs(r, col-1, atl, heights[r][col-1])
 
-        # find duplicate value from set 
-        print("check pac" , len(pac))
-        print("check alt", len(atl))
-        
-        result = []
-        
-        for p in pac:
-            for a in atl:
-                if p == a:
-                    result.append(p)
-        
-        print("check result", result)
-        return result
+        # print("check pac", pac)
+        # print("check atl", atl)
 
+        res = []
+
+        for r in range(row):
+            for c in range(col):
+                if ((r,c) in atl) and ((r,c) in pac):
+                    res.append([r,c])
+
+        return res
+                    
