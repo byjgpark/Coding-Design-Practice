140a2862d65918827b3c22bd189cd2d00e8091ac
Jungi Park <byjungy93@gmail.com>
2024-12-03 23:03:32 +0900

    [Working] LC_Medium_Course Scheudle_D10

--- LeetCode/LC_Medium_Course Schedule (2).py
+++ LeetCode/LC_Medium_Course Schedule (2).py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
@@ -17,21 +19,34 @@ class Solution:
 
         # def dfs(crs):
 
-            # if crs in visit: 
+        #     if crs in visit:
+        #         return False
+            
+        #     if  
 
 
 
-        # looping through the course number 
+        # # looping through the course number 
 
         # for i in range(numCourses):
 
-            
+        #     if not dfs(i):
+                
 
             
             # print("check i", i)
 
-
-
+if __name__ == "__main__":
+    sol = Solution()
+    
+    # generate cycle test case w/o cycle
+    # print(sol.canFinish(4, [[1, 0], [2, 1], [3, 2]]))
+    
+    # Cycle test case with cycle
+    # print("checking final result", sol.canFinish(4, [[1, 0], [2, 1], [3, 2], [1, 3]]))
+    
+    # generate another test case with cycle w/ 5 numCourses
+    print(sol.canFinish(5, [[1, 0], [2, 1], [3, 2], [4, 3], [3, 4]]))
 
 
         
