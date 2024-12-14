c1dc3e5573751a839fc931a73411bd2ae6ac45e1
Jungi Park <byjungy93@gmail.com>
2024-12-14 12:02:50 +0900

    [Working] LC_Medium_Course Scheudle_D17

--- LeetCode/LC_Medium_Course Schedule (2).py
+++ LeetCode/LC_Medium_Course Schedule (2).py
@@ -2,62 +2,57 @@ from typing import List
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
-        print("check numCourses :", numCourses)
 
-        prerequMap = {}
+        # print("Check prerequisites ", prerequisites)
 
+        preMap = {}
         for i in range(numCourses):
-            prerequMap[i] = []
+            preMap[i] =[]
 
         for crs, pre in prerequisites:
-            prerequMap[crs].append(pre)
-
-        print("check prerequMap", prerequMap)
-
+            preMap[crs].append(pre)
+        #   print("Check crs", crs, "pre", pre)  
+        print("check preMap", preMap)
+        
         visit = set()
-
+        
         def dfs(crs):
-
-            if crs in visit: 
+            
+            if crs in visit:
                 return False
             
-            if prerequMap[crs] == []:
+            if preMap[crs] == []:
                 return True
             
             visit.add(crs)
             
-            for pre in prerequMap[crs]:
-                
-                print("check pre", pre, "of crs", crs)
-                
-                
-                
-            
-
-
-        # # looping through the course number 
-
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    visit.remove(pre)
+                    return False
+ 
+            visit.remove(crs)
+            preMap[crs] = []
+            return True   
+        
         for i in range(numCourses):
-            dfs(i)
-
-        #     if not dfs(i):
-                
-
             
-            # print("check i", i)
-
+            if not dfs(i):
+                return False
+            
+        return True
+        
 if __name__ == "__main__":
     sol = Solution()
     
     # generate cycle test case w/o cycle
-    print(sol.canFinish(4, [[1, 0], [2, 1], [3, 2]]))
+    # print(sol.canFinish(4, [[1, 0], [2, 1], [3, 2]]))
     
     # Cycle test case with cycle
     # print("checking final result", sol.canFinish(4, [[1, 0], [2, 1], [3, 2], [1, 3]]))
     
     # generate another test case with cycle w/ 5 numCourses
-    # print(sol.canFinish(5, [[1, 0], [2, 1], [3, 2], [4, 3], [3, 4]]))
+    print(sol.canFinish(5, [[1, 0], [2, 1], [3, 2], [4, 3], [3, 4]]))
     
 
 
