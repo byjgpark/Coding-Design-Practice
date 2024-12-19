a2164e99209b3ffe410dd51b5c827c71061d88a1
Jungi Park <byjungy93@gmail.com>
2024-12-19 22:43:11 +0900

    [Finished] LC_Medium_Group Anagrams_D2

--- LeetCode/LC_Medium_Group Anagrams.py
+++ LeetCode/LC_Medium_Group Anagrams.py
@@ -1,7 +1,18 @@
+from typing import List
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)
+        
+        print("check res", res)
+        
         for s in strs:
             sortedS = ''.join(sorted(s))
             res[sortedS].append(s)
         return list(res.values())
+    
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
