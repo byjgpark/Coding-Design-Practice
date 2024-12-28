1031ee8e234d5f9c591b5c69a633dd11bd9f0fc5
Jungi Park <byjungy93@gmail.com>
2024-12-28 22:27:58 +0900

    [Working] LC_Medium_Group Anagrams_D8

--- LeetCode/LC_Medium_Group Anagrams (1).py
+++ LeetCode/LC_Medium_Group Anagrams (1).py
@@ -1,16 +1,31 @@
 from typing import List
 from collections import defaultdict
 
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         res = defaultdict(list)
+#         for s in strs:
+#             count = [0] * 26
+#             for c in s:
+#                 count[ord(c) - ord('a')] += 1
+#             res[tuple(count)].append(s)
+#         return list(res.values()) 
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
+        res = {}
         for s in strs:
             count = [0] * 26
             for c in s:
-                count[ord(c) - ord('a')] += 1
-            res[tuple(count)].append(s)
-        return list(res.values()) 
-    
+                ordFirst = ord(c)
+                ordSecond = ord('a')
+                ordThird = ordFirst - ordSecond
+                count[ordThird] += 1
+            key = tuple(count)
+            if key not in res:
+                res[key] = []
+            res[key].append(s)
+        return list(res.values())    
     
 if __name__ == "__main__":
     strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
