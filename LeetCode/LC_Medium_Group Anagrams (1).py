5bb5a24d881b1dcc6ad2775bd254e1c6a91bd646
Jungi Park <byjungy93@gmail.com>
2025-01-01 16:46:05 +0900

    [Working] LC_Medium_Group Anagrams_D10

--- LeetCode/LC_Medium_Group Anagrams (1).py
+++ LeetCode/LC_Medium_Group Anagrams (1).py
@@ -1,33 +1,30 @@
-from typing import List
-from collections import defaultdict
-
-# class Solution:
-#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-#         res = defaultdict(list)
-#         for s in strs:
-#             count = [0] * 26
-#             for c in s:
-#                 count[ord(c) - ord('a')] += 1
-#             res[tuple(count)].append(s)
-#         return list(res.values()) 
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = {}
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                ordFirst = ord(c)
-                ordSecond = ord('a')
-                ordThird = ordFirst - ordSecond
-                count[ordThird] += 1
-            key = tuple(count)
-            if key not in res:
-                res[key] = []
-            res[key].append(s)
-        return list(res.values())    
-    
-    
-if __name__ == "__main__":
-    strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-    print(Solution().groupAnagrams(strs)) 
+        
+        map = {}
+
+        for str in strs:    
+
+            arr = [0] * 26    
+
+            # print("check arr here ", arr)        
+
+            for s in str:
+
+                # print("check s :",s,"ord(s) :", ord(s))
+                diffOrd = ord(s) - ord('a')
+                # print("check diffOrd :", diffOrd)
+                arr[diffOrd] +=1
+            
+            tupleArr = tuple(arr)
+            if tupleArr not in map:
+                map[tupleArr] = []
+            
+            map[tupleArr].append(str)   
+
+            # print("check str ", str, "arr :", arr)  
+            # print("check map object :", map)      
+            # print("check map", list(map.values()))      
+        
+        return list(map.values())   
+
