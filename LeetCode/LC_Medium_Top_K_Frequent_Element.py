85236978850fc08d3d7f8fc3afc4935f46edb4ca
Jungi Park <byjungy93@gmail.com>
2025-01-09 22:33:38 +0900

    [Working] LC_Medium_Group Anagrams_D18

---
+++ LeetCode/LC_Medium_Top_K_Frequent_Element.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+        
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+                
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.topKFrequent([1,1,1,2,2,3], 2))   
+    # print(sol.topKFrequent([1], 1))
