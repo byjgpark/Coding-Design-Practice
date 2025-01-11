d36532c2a03fc5ed06ebe91d1cf6057091d8b15d
Jungi Park <byjungy93@gmail.com>
2025-01-11 09:01:58 +0900

    [Working] LC_Medium_Top_K_Frequent_Element_D1

--- LeetCode/LC_Medium_Top_K_Frequent_Element.py
+++ LeetCode/LC_Medium_Top_K_Frequent_Element.py
@@ -1,23 +1,45 @@
-from typing import List
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums) + 1)]
-
-        for num in nums:
-            count[num] = 1 + count.get(num, 0)
-        for num, cnt in count.items():
-            freq[cnt].append(num)
         
+        map = {}
+        for num in nums:
+            if num in map:
+                map[num] += 1
+            else:
+                map[num] = 1
+    
+        print("check map", map)
+
+        arr = []
+
+        for _ in nums:
+            arr.append([])
+
+        print("check arr", arr)
+
+        # print("Check map.item()", map.items())
+
+        for num, cnt in map.items():
+            arr[cnt-1].append(num)
+
+        print("check final arr" ,arr)
+
+        i = len(nums)-1
+
+        print("check final i" ,i)
+
         res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for num in freq[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
-                
-if __name__ == "__main__":
-    sol = Solution()
-    print(sol.topKFrequent([1,1,1,2,2,3], 2))   
-    # print(sol.topKFrequent([1], 1))
+
+        while i >= 0:
+            print("check i", i)
+            if arr[i]:
+                for num in arr[i]:
+                    print("Check num", num)
+                    if num == k:
+                        
+            i-=1
+
+
+        
+        
+        
