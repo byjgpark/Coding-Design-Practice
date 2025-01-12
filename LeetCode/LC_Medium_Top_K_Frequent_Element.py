3dd6d3bbf5cd60475ddfd7f5cda3b8e4c229dfd2
Jungi Park <byjungy93@gmail.com>
2025-01-12 09:29:00 +0900

    [Finished] LC_Medium_Top_K_Frequent_Element_D1

--- LeetCode/LC_Medium_Top_K_Frequent_Element.py
+++ LeetCode/LC_Medium_Top_K_Frequent_Element.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
@@ -11,11 +13,15 @@ class Solution:
         print("check map", map)
 
         arr = []
+        
+        freq = [[] for i in range(len(nums) + 1)]
 
         for _ in nums:
             arr.append([])
 
         print("check arr", arr)
+    
+        print("check freq", freq)
 
         # print("Check map.item()", map.items())
 
@@ -31,13 +37,26 @@ class Solution:
         res = []
 
         while i >= 0:
-            print("check i", i)
             if arr[i]:
                 for num in arr[i]:
                     print("Check num", num)
-                    if num == k:
-                        
+                    res.append(num)
+                    if len(res) == k:
+                        return res
+                    
+            print("check i : ",i)
             i-=1
+
+if __name__ == "__main__":
+    # nums = [1,1,1,2,2,3]
+    # k = 2
+    
+    nums = [1]
+    k = 1 
+    
+    print(Solution().topKFrequent(nums, k))
+
+            
 
 
         
