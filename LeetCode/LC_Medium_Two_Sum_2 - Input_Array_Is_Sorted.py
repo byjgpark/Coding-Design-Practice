b5164c244d613801d12ad6bf319fbd7c7756bb20
Jungi Park <byjungy93@gmail.com>
2025-01-14 22:23:20 +0900

    [Working] LC_Medium_Two_Sum_2 - Input_Array_Is_Sorted_D2

--- LeetCode/LC_Medium_Two_Sum_2 - Input_Array_Is_Sorted.py
+++ LeetCode/LC_Medium_Two_Sum_2 - Input_Array_Is_Sorted.py
@@ -1,3 +1,8 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
+        for i in range(len(numbers)):
+            print("num :", i)
+
+        for i in range(5, -len(numbers)-1, -1):  
+            print("check", i)
