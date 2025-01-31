247a261334e92e9e269876ef773e2e5840a8dc2b
Jungi Park <byjungy93@gmail.com>
2025-01-31 15:01:46 +0900

    [Working] LC_Medium_Two_Min_Cost_Climbing_Stairs_D1

--- LeetCode/LC_Medium_Two_Climbing_Stairs_Space_Optimized.py
+++ LeetCode/LC_Medium_Two_Climbing_Stairs_Space_Optimized.py
@@ -3,6 +3,7 @@ class Solution:
         one, two = 1, 1
 
         for i in range(n - 1):
+            print("check i", i)
             temp = one
             one = one + two
             two = temp
