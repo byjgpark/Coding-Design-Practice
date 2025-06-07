4d73bce9f9ff94ce9cefcc293e87b4f421c35204
Jungi Park <byjungy93@gmail.com>
2025-06-07 09:15:16 +0900

    [Working] LC_Medium_Valid Sudoku_D8

--- LeetCode/LC_Medium_Valid Sudoku.py
+++ LeetCode/LC_Medium_Valid Sudoku.py
@@ -33,4 +33,62 @@ class Solution:
         return True
     
     
+    # First try
+    class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        rows = len(board)
+        cols = len(board[0])
+
+        print("check row = ", rows, "col = ", cols)
+
+        #checking row 
+        for row in range(rows):
+            seen = set()
+            for i in range(9):
+                # print("check row = ", row, " i = ", i)
+
+                if board[row][i] == ".":
+                    continue
+                if board[row][i] in seen:
+                    return False
+                seen.add(board[row][i])
+
+        print("check row seen = ", seen)
+
+        #checking column
+        for col in range(cols):
+            seen = set()
+            for i in range(9):
+                # print("check row = ", row, " i = ", i)
+                if board[i][col] == ".":
+                    continue
+                if board[i][col] in seen:
+                    return False
+                seen.add(board[i][col])
+        
+        print("check column seen = ", seen)
+
+        #checking sub-box
+        for box in range(9):
+            seen = set()
+            for i in range(rows):
+                for j in range(cols):
+                    row = (box // 3) * 3 + i
+                    col = (box % 3) * 3 + j
+
+                    print("check row = ", row, " col = ", col)
+                    if board[row][col] == ".":
+                        continue
+                    if board[row][col] in seen:
+                        return False
+                    seen.add(board[row][col])
+
+        return True
+
+                    
+
+                    
+                    
+    
     
