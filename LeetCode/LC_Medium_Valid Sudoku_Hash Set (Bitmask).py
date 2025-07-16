52097e7adbac07d784f47851370693ba03a11691
Jungi Park <byjungy93@gmail.com>
2025-07-16 21:40:04 +0900

    LC_Medium_Valid Sudoku_Hash Set (Bitmask)_D11

--- LeetCode/LC_Medium_Valid Sudoku_Hash Set (Bitmask).py
+++ LeetCode/LC_Medium_Valid Sudoku_Hash Set (Bitmask).py
@@ -47,6 +47,7 @@ class Solution:
                     print(f"  !!! Duplicate {digit} found in square {square_index}")
                     return False
 
+
                 # Update bitmasks
                 rows[r] |= pos
                 cols[c] |= pos
