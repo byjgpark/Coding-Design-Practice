123f5c0e7497285c4407d27360b2dde4853fca7d
Jungi Park <byjungy93@gmail.com>
2025-08-20 09:10:03 +0900

    [Working] LC_Medium_Longest Consecutive Sequence_Sorting_Hash Map_D1

---
+++ LeetCode/LC_Medium_Longest Consecutive Sequence_Sorting_Hash Map.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        mp = defaultdict(int)
+        res = 0
+
+        print("check mp =", mp)
+
+        for num in nums:
+            if not mp[num]:
+                mp[num] = mp[num - 1] + mp[num + 1] + 1
+                mp[num - mp[num - 1]] = mp[num]
+                mp[num + mp[num + 1]] = mp[num]
+                res = max(res, mp[num])
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.longestConsecutive([100, 4, 200, 1, 3, 2]))
