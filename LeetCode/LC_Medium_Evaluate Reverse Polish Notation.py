e71f293d5312532fada7d1c0094f9e8286808ae3
Jungi Park <byjungy93@gmail.com>
2025-10-29 08:49:41 +0900

    [Working] System Design_D4

--- LeetCode/LC_Medium_Evaluate Reverse Polish Notation.py
+++ LeetCode/LC_Medium_Evaluate Reverse Polish Notation.py
@@ -1,5 +1,7 @@
 tokens = ["2", "1", "+", "3", "*"]
 
+tokens = ["3", "3", "*"]
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         while len(tokens) > 1:
@@ -18,3 +20,7 @@ class Solution:
                     tokens = tokens[:i-2] + [str(result)] + tokens[i+1:]
                     break
         return int(tokens[0])
+
+## whats its time and space compelxity?
+## time: O(n)
+## space: O(n)
