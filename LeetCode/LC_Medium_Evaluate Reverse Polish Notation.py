2c1bb308e2394baea22255667f9b3e9f7ce7a469
Jungi Park <byjungy93@gmail.com>
2025-10-29 09:47:06 +0900

    [Working] LC_Medium_Evaluate Reverse Polish Notation_D4

--- LeetCode/LC_Medium_Evaluate Reverse Polish Notation.py
+++ LeetCode/LC_Medium_Evaluate Reverse Polish Notation.py
@@ -1,26 +1,38 @@
-tokens = ["2", "1", "+", "3", "*"]
+# tokens = ["2", "1", "+", "3", "*"]
 
-tokens = ["3", "3", "*"]
+# tokens = ["3", "3", "*"]
 
+# class Solution:
+#     def evalRPN(self, tokens: List[str]) -> int:
+#         while len(tokens) > 1:
+#             for i in range(len(tokens)):
+#                 if tokens[i] in "+-*/":
+#                     a = int(tokens[i-2])
+#                     b = int(tokens[i-1])
+#                     if tokens[i] == '+':
+#                         result = a + b
+#                     elif tokens[i] == '-':
+#                         result = a - b
+#                     elif tokens[i] == '*':
+#                         result = a * b
+#                     elif tokens[i] == '/':
+#                         result = int(a / b)
+#                     tokens = tokens[:i-2] + [str(result)] + tokens[i+1:]
+#                     break
+#         return int(tokens[0])
+
+# ## whats its time and space compelxity?
+# ## time: O(n)
+# ## space: O(n)
+
+# 2nd trial 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        while len(tokens) > 1:
-            for i in range(len(tokens)):
-                if tokens[i] in "+-*/":
-                    a = int(tokens[i-2])
-                    b = int(tokens[i-1])
-                    if tokens[i] == '+':
-                        result = a + b
-                    elif tokens[i] == '-':
-                        result = a - b
-                    elif tokens[i] == '*':
-                        result = a * b
-                    elif tokens[i] == '/':
-                        result = int(a / b)
-                    tokens = tokens[:i-2] + [str(result)] + tokens[i+1:]
-                    break
-        return int(tokens[0])
+        
+        print("check tokens =", tokens)
 
-## whats its time and space compelxity?
-## time: O(n)
-## space: O(n)
+        print("check tokens =", len(tokens))
+
+        while len(tokens) > 1:
+            for i in range(tokens):
+                if tokens[i] == "+":
