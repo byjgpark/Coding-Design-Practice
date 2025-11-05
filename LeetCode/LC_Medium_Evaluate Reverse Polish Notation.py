d63404a01104c5f273204324794758badb8826da
Jungi Park <byjungy93@gmail.com>
2025-11-05 09:23:21 +0900

    [Working] LC_Medium_Evaluate Reverse Polish Notation_D8

--- LeetCode/LC_Medium_Evaluate Reverse Polish Notation.py
+++ LeetCode/LC_Medium_Evaluate Reverse Polish Notation.py
@@ -86,6 +86,38 @@ class Solution:
                         result = a/b 
                         print("check tokens", i)
 
+# 5th trial
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        
+        i = 2
+        # first = tokens[:i-2]
+        # second = tokens[i+1:]
+        res=0
+        # print("Check first =", first, " second =", second)
+
+        while len(tokens)>1:
+            for i in range(len(tokens)):
+                # print("Check i =", i) 
+                if tokens[i] in "+-*/":
+                    a=int(tokens[i-2])
+                    b=int(tokens[i-1])
+                    if tokens[i] == "+":
+                        res=a+b
+                    elif tokens[i] == "-":
+                        res=a-b
+                    elif tokens[i] == "*":
+                        res=a*b
+                    elif tokens[i] == "/":
+                        res=int(a/b)
+                        # print("check res =",res)
+                        # print("check abs(a//b) =", abs(a//b))
+                    tokens = tokens[:i-2] + [str(res)] + tokens[i+1:]
+                    # print("Check tokens =", tokens)
+                    break
+
+        return int(tokens[0])
+                    
             
 
                 
