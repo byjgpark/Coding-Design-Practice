595ed921c965f4cb1ed53699abea2a21bb1e904e
Jungi Park <byjungy93@gmail.com>
2025-11-12 09:19:06 +0900

    [Working] LC_Medium_Evaluate Reverse Polish Notation_Stack_D5

--- LeetCode/LC_Medium_Evaluate Reverse Polish Notation_stack.py
+++ LeetCode/LC_Medium_Evaluate Reverse Polish Notation_stack.py
@@ -1,3 +1,4 @@
+# 1st trial
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
@@ -16,6 +17,36 @@ class Solution:
                 stack.append(int(c))
         return stack[0]
 
+
+# 2nd trial
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
         
+        stack = []
+        for token in tokens:
+            
+            if token == "+":
+                a, b = stack.pop(), stack.pop()
+                stack.append(a+b)
+
+            elif token == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b-a)
+
+            elif token == "*":
+                a, b = stack.pop(), stack.pop()
+                stack.append(a*b)
+
+            elif token == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b/a))
+
+            else:
+                stack.append(int(token))
+
+            # print("check stack =", stack)
+            # print("Check token =", token)
+        return stack[0]
+                    
 
         
