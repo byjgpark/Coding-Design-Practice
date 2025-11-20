2e9bc69234001502b63efa3fb373196e5f196283
Jungi Park <byjungy93@gmail.com>
2025-11-20 09:31:45 +0900

    [Working] LC_Medium_Koko Eating Bananas_D4

--- LeetCode/LC_Medium_Koko Eating Bananas.py
+++ LeetCode/LC_Medium_Koko Eating Bananas.py
@@ -16,3 +16,27 @@ class Solution:
                 return speed
             speed += 1
         return speed
+
+
+## 3rd trial
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        k = 1
+        while True:
+            res = 0    
+            print("check k =",k)
+            for pile in piles:
+                # print("check pile =", pile)
+                # print(" ceil =" ,math.ceil(pile/k))
+                res += math.ceil(pile/k)
+                print("Check res =", res)
+            # print("Check res =", res)
+
+            k+=1
+            if res<=h:
+                return res
+            
+
+        return res
+            
