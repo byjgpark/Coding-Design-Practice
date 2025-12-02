ef6de37c7cdcb67eb07ccb15da71ff4a318daf06
Jungi Park <byjungy93@gmail.com>
2025-12-02 09:30:28 +0900

    [Working] LC_Medium_Koko Eating Bananas_Binary Search_D6

--- LeetCode/LC_Medium_Koko Eating Bananas_Binary Search.py
+++ LeetCode/LC_Medium_Koko Eating Bananas_Binary Search.py
@@ -16,7 +16,31 @@ class Solution:
                 l = k + 1
         return res
 
+    
+## 2nd trial
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
         
+        l, r = 1, max(piles)
+
+        print("check l =",l," r =", r)
+
+        res=r
+    
+        while l<=r:
+
+            k=(l+r)//2
+
+            print("check k =", k)
+            
+            remaining = 0
+            for pile in piles:
+                remaining+=math.ceil(pile/k)
+                # print("Check pile =", pile)
+                # print("Check ceil =", math.ceil(1.2))
+                print("Check remaining =", remaining)
+
+
 
 
         
